8d9b68585e92738d74af25b38b73662fc3e2abe2
Yakovlev Andrew <drozy@yandex.ru>
2024-03-12 16:20:19 +0300

    for local start and tests

--- cookbook/settings.py
+++ cookbook/settings.py
@@ -130,7 +130,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 LOGIN_URL = '/auth/login/'
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
